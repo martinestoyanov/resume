0a029e0dc26a0cabeb3e43a40413e4700930865c
Martin Stoyanov <martinestoyanov@gmail.com>
2021-08-05 23:45:05 -0400

    updated hbs partials to match theme

---
+++ boilerplate/index.js
@@ -0,0 +1,57 @@
+var fs = require("fs");
+var path = require("path");
+var Handlebars = require("handlebars");
+
+function render(resume) {
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+  var partialsDir = path.join(__dirname, "partials");
+  var filenames = fs.readdirSync(partialsDir);
+
+  filenames.forEach(function (filename) {
+    var matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+      return;
+    }
+    var name = matches[1];
+    var filepath = path.join(partialsDir, filename);
+    var template = fs.readFileSync(filepath, "utf8");
+
+    Handlebars.registerPartial(name, template);
+  });
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume,
+  });
+}
+
+Handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+  switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
+
+module.exports = {
+  render: render,
+};
